f7d0616dff75a983a0d5bc4cd776b232552d989f
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2022-12-13 14:11:59 -0300

    .

--- src/RoutesDir.js
+++ src/RoutesDir.js
@@ -1,12 +1,12 @@
-import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, Navigate, Link} from 'react-router-dom';
 import Main from './pages/main/Main';
 import RoutesCore from './RoutesCore';
 
 function RoutesDir() {
   return <Router>
     <Routes>
-      <Route path="/" element={<Main/>}/>
-      {RoutesCore.length && RoutesCore.map(({path, Element, exact = !1}, ind) => <Route key={ind} path={path} element={<Element/>} exact={exact}/>)}
+      <Route path="/" element={<Main/>} exact/>
+      {RoutesCore.length && RoutesCore.map(({path, Element, exact = !1}, ind) => <Route key={ind} path={path} element={<><Link to="/">Voltar para Main</Link><Element/></>} exact={exact}/>)}
       <Route path="*" element={<Navigate to="/"/>}/>
     </Routes>
   </Router>
--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+import RoutesCore from "../../RoutesCore";
+
 function Main() {
-    return <div>Main</div>
+    return <>
+        <div>Main</div>
+        {RoutesCore.length && RoutesCore.map(({path}, ind) => <>
+        <Link key={ind} to={path}>{path}</Link>
+        <br></br>
+        </>)}
+    </>
 };
 
 export default Main;
