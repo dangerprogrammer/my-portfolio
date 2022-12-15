4357b351d99d45948042a118af2d2620cf545f14
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2022-12-15 01:39:08 -0300

    .

--- src/RoutesCore.js
+++ src/RoutesCore.js
@@ -2,10 +2,10 @@ import {AboutMe, AboutMeDefault} from "./pages/about-me/AboutMe";
 import {Skills, SkillsDefault} from "./pages/skills/Skills";
 import {MyProjects, MyProjectsDefault} from "./pages/my-projects/MyProjects";
 
-const routesCore = [
+const RoutesCore = [
     {path: '/about-me', Element: AboutMe, DefaultElem: AboutMeDefault},
     {path: '/skills', Element: Skills, DefaultElem: SkillsDefault},
     {path: '/my-projects', Element: MyProjects, DefaultElem: MyProjectsDefault}
 ];
 
-export default routesCore;
+export default RoutesCore;
--- src/RoutesDir.js
+++ src/RoutesDir.js
@@ -1,12 +1,12 @@
 import {BrowserRouter as Router, Routes, Route, Navigate, Link} from 'react-router-dom';
 import Main from './pages/main/Main';
-import routesCore from './routesCore';
+import RoutesCore from './RoutesCore';
 
 function RoutesDir() {
   return <Router>
     <Routes>
       <Route path="/" element={<Main/>} exact/>
-      {routesCore.length && routesCore.map(({path, Element, exact = !1}, ind) => <Route key={ind} path={path} element={<><Link to="/">Voltar para Main</Link><Element/></>} exact={exact}/>)}
+      {RoutesCore.length && RoutesCore.map(({path, Element, exact = !1}, ind) => <Route key={ind} path={path} element={<><Link to="/">Voltar para Main</Link><Element/></>} exact={exact}/>)}
       <Route path="*" element={<Navigate to="/"/>}/>
     </Routes>
   </Router>
