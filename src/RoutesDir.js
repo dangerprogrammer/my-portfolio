1e7a325db17ac4d4eae6907baec83ac5c11d0797
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2022-12-15 01:36:55 -0300

    .

--- src/RoutesDir.js
+++ src/RoutesDir.js
@@ -1,15 +1,6 @@
 import {BrowserRouter as Router, Routes, Route, Navigate, Link} from 'react-router-dom';
 import Main from './pages/main/Main';
-// import routesCore from './routesCore';
-import {AboutMe, AboutMeDefault} from "./pages/about-me/AboutMe";
-import {Skills, SkillsDefault} from "./pages/skills/Skills";
-import {MyProjects, MyProjectsDefault} from "./pages/my-projects/MyProjects";
-
-const routesCore = [
-    {path: '/about-me', Element: AboutMe, DefaultElem: AboutMeDefault},
-    {path: '/skills', Element: Skills, DefaultElem: SkillsDefault},
-    {path: '/my-projects', Element: MyProjects, DefaultElem: MyProjectsDefault}
-];
+import routesCore from './routesCore';
 
 function RoutesDir() {
   return <Router>
