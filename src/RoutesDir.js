911f46327411d1c2e9429a53c42bf184818e740c
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2022-12-15 01:27:40 -0300

    .

--- src/RoutesDir.js
+++ src/RoutesDir.js
@@ -1,6 +1,15 @@
 import {BrowserRouter as Router, Routes, Route, Navigate, Link} from 'react-router-dom';
 import Main from './pages/main/Main';
-import routesCore from './routesCore';
+// import routesCore from './routesCore';
+import {AboutMe, AboutMeDefault} from "./pages/about-me/AboutMe";
+import {Skills, SkillsDefault} from "./pages/skills/Skills";
+import {MyProjects, MyProjectsDefault} from "./pages/my-projects/MyProjects";
+
+const routesCore = [
+    {path: '/about-me', Element: AboutMe, DefaultElem: AboutMeDefault},
+    {path: '/skills', Element: Skills, DefaultElem: SkillsDefault},
+    {path: '/my-projects', Element: MyProjects, DefaultElem: MyProjectsDefault}
+];
 
 function RoutesDir() {
   return <Router>
--- src/components/main-pages-list/MainPagesList.js
+++ src/components/main-pages-list/MainPagesList.js
@@ -1,6 +1,15 @@
 import { Link } from "react-router-dom";
 import PageContent from "../page-content/PageContent";
-import routesCore from "../../routesCore";
+// import routesCore from "../../routesCore";
+import {AboutMe, AboutMeDefault} from "../../pages/about-me/AboutMe";
+import {Skills, SkillsDefault} from "../../pages/skills/Skills";
+import {MyProjects, MyProjectsDefault} from "../../pages/my-projects/MyProjects";
+
+const routesCore = [
+    {path: '/about-me', Element: AboutMe, DefaultElem: AboutMeDefault},
+    {path: '/skills', Element: Skills, DefaultElem: SkillsDefault},
+    {path: '/my-projects', Element: MyProjects, DefaultElem: MyProjectsDefault}
+];
 
 function MainPagesList() {
     return <div>{routesCore.length && routesCore.map(({path, DefaultElem}, ind) => <MainPagesItem key={ind} path={path} Element={DefaultElem}/>)}</div>
