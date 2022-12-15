f65dd60c15863ae3e8729d02abf06d3ad8ccd8a4
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2022-12-15 01:45:09 -0300

    .

--- src/RoutesCore.js
+++ src/RoutesCore.js
@@ -3,9 +3,24 @@ import {Skills, SkillsDefault} from "./pages/skills/Skills";
 import {MyProjects, MyProjectsDefault} from "./pages/my-projects/MyProjects";
 
 const RoutesCore = [
-    {path: '/about-me', Element: AboutMe, DefaultElem: AboutMeDefault},
-    {path: '/skills', Element: Skills, DefaultElem: SkillsDefault},
-    {path: '/my-projects', Element: MyProjects, DefaultElem: MyProjectsDefault}
+    {path: '/about-me', Element() {
+        return <>
+            <AboutMeDefault/>
+            <AboutMe/>
+        </>
+    }, DefaultElem: AboutMeDefault},
+    {path: '/skills', Element() {
+        return <>
+            <SkillsDefault/>
+            <Skills/>
+        </>
+    }, DefaultElem: SkillsDefault},
+    {path: '/my-projects', Element() {
+        return <>
+            <MyProjectsDefault/>
+            <MyProjects/>
+        </>
+    }, DefaultElem: MyProjectsDefault}
 ];
 
 export default RoutesCore;
