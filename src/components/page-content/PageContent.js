a3bc378865b72d790a217c335a45e99c9ac220ed
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2022-12-21 12:10:55 -0300

    .

--- src/components/page-content/PageContent.js
+++ src/components/page-content/PageContent.js
@@ -1,7 +1,7 @@
 import './PageContent.scss';
 
 function PageContent({children, subContent, id}) {
-    return <section data-id="page-content" id={id} className={subContent}>{children}</section>
+    return <section data-id="page-content" id={id}>{children}</section>
 };
 
 export default PageContent;
