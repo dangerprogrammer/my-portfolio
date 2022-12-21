1a78e779ec299d24aaf3a445a0602c5c990df107
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2022-12-21 11:27:07 -0300

    .

--- src/scripts/activePage.js
+++ src/scripts/activePage.js
@@ -2,11 +2,11 @@ function activePage(page) {
     const aTarget = page.parentElement, aSiblings = [...aTarget.parentElement.children],
         {origin, pathname} = window.location, href = aTarget.href.slice((origin + pathname).length),
         targetSection = document.querySelector(`section${href}`), sectionTop = targetSection.offsetTop,
-        pageContainer = document.querySelector('#page-container'), scrollDown = document.querySelector('#scroll-down');
+        pageContainer = document.querySelector('#page-container'), scrollDown = document.querySelector('#scroll-down'), locationHash = window.location.hash;
 
     aSiblings.forEach(sibling => sibling.classList[sibling.href === aTarget.href ? 'add' : 'remove']('active'));
-    
-    if (sectionTop > 0) page.click();
+
+    if (sectionTop > 0 || locationHash) page.click();
     
     scrollDown.classList.toggle('scrolled', sectionTop > 0);
     pageContainer.scrollTo(0, sectionTop);
@@ -16,8 +16,8 @@ function activeNextPage() {
     const ownerA = document.querySelector(`#nav-page .active`),
         siblings = [...ownerA.parentElement.children];
     let nextPage;
-    siblings.forEach((sibling, ind) => {
-        if (sibling.href === ownerA.href) nextPage = siblings[Math.min(ind + 1, siblings.length - 1)].firstChild;
+    siblings.forEach(({href}, ind) => {
+        if (href === ownerA.href) nextPage = siblings[Math.min(ind + 1, siblings.length - 1)].firstChild;
     });
 
     return activePage(nextPage);
@@ -27,8 +27,8 @@ function activePrevPage() {
     const ownerA = document.querySelector(`#nav-page .active`),
         siblings = [...ownerA.parentElement.children];
     let prevPage;
-    siblings.forEach((sibling, ind) => {
-        if (sibling.href === ownerA.href) prevPage = siblings[Math.max(ind - 1, 0)].firstChild;
+    siblings.forEach(({href}, ind) => {
+        if (href === ownerA.href) prevPage = siblings[Math.max(ind - 1, 0)].firstChild;
     });
 
     return activePage(prevPage);
