b50cb62b62d99d87abe1b3045dc4d8fd4873eb5c
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-08-27 16:12:21 -0300

    .

---
+++ assets/socialMedias.js
@@ -0,0 +1,9 @@
+import LinkedIn from '@/assets/svgs/logo-linkedin.svg';
+import GitHub from '@/assets/svgs/logo-github.svg';
+
+const socialMedias = [
+    { link: 'https://github.com/dangerprogrammer', Icon: GitHub },
+    { link: 'https://www.linkedin.com/in/patrick-vieira-leo', Icon: LinkedIn }
+];
+
+export default socialMedias;
--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,8 +1,19 @@
-import { navbarStyles } from './Navbar.module.scss';
+import socialMedias from '@/assets/socialMedias';
+import PreLoader from '../preloader/PreLoader';
+import { navbarStyles, socialMedia } from './Navbar.module.scss';
+import Link from 'next/link';
 
 function Navbar() {
     return <nav className={navbarStyles}>
-        Navbar
+        <PreLoader/>
+        <PreLoader transitionDelay='.15s' opacity='.5'/>
+        <PreLoader transitionDelay='.25s' opacity='.5'/>
+        <h1>Patrick Vieira Léo</h1>
+        <ul className={socialMedia}>
+            {socialMedias.map(({ link, Icon }, ind) => <li key={ind}>
+                <Link href={link}><Icon/></Link>
+            </li>)}
+        </ul>
     </nav>
 };
 
