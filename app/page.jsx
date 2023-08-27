d2eb7f5c9f91c4c7a89efacc86d7c8f0b6a0c6c5
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-08-27 03:08:31 -0300

    .

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@
 
 import AboutMe from "@/components/about-me/AboutMe";
 import MyProjects from "@/components/my-projects/MyProjects";
-import Navbar from "@/components/navbar/Navbar";
+// import Navbar from "@/components/navbar/Navbar";
 import Page from "@/components/page/Page";
 import Skills from "@/components/skills/Skills";
 import Welcome from "@/components/welcome/Welcome";
@@ -10,6 +10,8 @@ import BackgroundCanvas from "@/components/background-canvas/BackgroundCanvas";
 import { renderScrolling } from "@/scripts/initializePageTools";
 import { useEffect } from "react";
 import Sidebar from "@/components/sidebar/Sidebar";
+import Navbar2 from "@/components/navbar2/Navbar2";
+import Navbar from "@/components/navbar/Navbar";
 
 function Home() {
   useEffect(() => {
@@ -17,7 +19,9 @@ function Home() {
   }, []);
 
   return <>
+    {/* <Navbar/> */}
     <Navbar/>
+    <Navbar2/>
     <Page>
       <Welcome/>
       <AboutMe/>
---
+++ components/navbar2/Navbar2.jsx
@@ -0,0 +1,7 @@
+import { navbarStyles } from './Navbar.module.scss';
+
+function Navbar2() {
+    return <nav className={navbarStyles}></nav>
+};
+
+export default Navbar2;
