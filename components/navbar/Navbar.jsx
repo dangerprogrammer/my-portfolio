60d8645f0b57d48dfbfd9cdf9799cd351bb0ea7c
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-08-27 19:32:25 -0300

    .

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,13 +1,15 @@
 import socialMedias from '@/assets/socialMedias';
 import PreLoader from '../preloader/PreLoader';
-import { navbarStyles, socialMedia } from './Navbar.module.scss';
+import { navbarStyles, socialMedia, preloaderContainer } from './Navbar.module.scss';
 import Link from 'next/link';
 
 function Navbar() {
     return <nav className={navbarStyles}>
-        <PreLoader/>
-        <PreLoader transitionDelay='.15s' opacity='.5'/>
-        <PreLoader transitionDelay='.25s' opacity='.5'/>
+        <span className={preloaderContainer}>
+            <PreLoader zIndex='2'/>
+            <PreLoader transitionDelay='.05s' backgroundColor='red' zIndex='1'/>
+            <PreLoader transitionDelay='.1s' backgroundColor='white' zIndex='0'/>
+        </span>
         <h1>Patrick Vieira Léo</h1>
         <ul className={socialMedia}>
             {socialMedias.map(({ link, Icon }, ind) => <li key={ind}>
