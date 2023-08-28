06afb3568b3125ed054cb0cbee75c4371ca2d0d5
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-08-27 22:59:26 -0300

    .

--- assets/navPages.js
+++ assets/navPages.js
@@ -1,10 +1,13 @@
 import Home from '@/assets/svgs/home-outline.svg';
+import AboutMe from '@/assets/svgs/person-outline.svg';
+import Tools from '@/assets/svgs/hammer-outline.svg';
+import Desktop from '@/assets/svgs/desktop-outline.svg';
 
 const navPages = [
     { link: '#welcome', text: 'Home', Icon: Home },
-    { link: '#about-me', text: 'About Me', Icon: Home },
-    { link: '#skills', text: 'Skills', Icon: Home },
-    { link: '#my-projects', text: 'My Projects', Icon: Home }
+    { link: '#about-me', text: 'About Me', Icon: AboutMe },
+    { link: '#skills', text: 'Skills', Icon: Tools },
+    { link: '#my-projects', text: 'My Projects', Icon: Desktop }
 ];
 
 export default navPages;
