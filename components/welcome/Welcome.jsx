1522d52b9cb94557468b8f94b631638031999cc5
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-08-30 01:00:48 -0300

    .

--- components/welcome/Welcome.jsx
+++ components/welcome/Welcome.jsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { welcomeStyles, imageContainer, textContainer, mainTitle, titleDesc } from './Welcome.module.scss';
+import { welcomeStyles, imageContainer, textContainer, mainTitle, titleDesc, titleName } from './Welcome.module.scss';
 import Wallpaper from '@/assets/images/abstract-wallpaper.jpg';
 
 function Welcome() {
@@ -7,10 +7,16 @@ function Welcome() {
         <article className={textContainer}>
             <h1 className={mainTitle}>
                 <div data-show><span>Hi, i'm <br /></span></div>
-                <div data-show><strong>Patrick</strong></div>
+                <div data-show className={titleName}><strong>Patrick</strong></div>
             </h1>
             <div data-show>
-                <h2 className={titleDesc}>Full-stack web developer</h2>
+                <h2 className={titleDesc}>Full-stack web dev</h2>
+            </div>
+            <div data-show>
+                <p>Do you want to know more about me?</p>
+            </div>
+            <div data-show>
+                <p>Scroll page for see more</p>
             </div>
         </article>
         <article className={imageContainer}>
