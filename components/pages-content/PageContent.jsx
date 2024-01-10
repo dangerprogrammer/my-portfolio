1f25840ac68b31f8708fc65bb995a7eae871183e
Patrick <104269678+dangerprogrammer@users.noreply.github.com>
2024-01-10 10:17:13 -0300

    .

--- components/pages-content/PageContent.jsx
+++ components/pages-content/PageContent.jsx
@@ -1,10 +1,13 @@
+import { useRouter } from 'next/navigation';
 import { pageContent, imageContainer, notClickStyles, preLoaders, contentImage, mainTitle } from './PageContent.module.scss';
 
-function PageContent({ titlePage, notClicking: notClick, children, ...contexts }) {
-    return <section {...contexts} style={{display: 'flex'}}>
+function PageContent({ titlePage, notClicking: notClick, children, id, ...contexts }) {
+    const router = useRouter();
+
+    return <section {...{id, ...contexts}} style={{display: 'flex'}}>
         <main className={pageContent} style={{zIndex: 50}}>
             <h1 className={mainTitle}>{titlePage}</h1>
-            <aside className={[imageContainer, notClick ? notClickStyles : undefined].join(' ')} onClick={() => console.log("opa!")}>
+            <aside className={[imageContainer, notClick ? notClickStyles : undefined].join(' ')} onClick={() => router.push(`/${id}`)}>
                 <div className={preLoaders} style={{'--delay': '.15s', backgroundColor: 'black', zIndex: 2}}></div>
                 <div className={preLoaders} style={{'--delay': '.075s', backgroundColor: 'red', zIndex: 1}}></div>
                 <div className={preLoaders} style={{'--delay': '0s', backgroundColor: 'white', zIndex: 0}}></div>
