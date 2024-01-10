0027afaf4dc4ebccda8d14075454d31179d15642
Patrick <104269678+dangerprogrammer@users.noreply.github.com>
2024-01-10 12:34:45 -0300

    .

--- app/(my-pages)/about-me/page.jsx
+++ app/(my-pages)/about-me/page.jsx
@@ -3,9 +3,14 @@
 import Navbar from "@/components/navbar/Navbar";
 import { renderSecPage } from "@/scripts/initializePageTools";
 import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 function AboutMe() {
+    const router = useRouter();
     useEffect(renderSecPage, []);
+
+    console.log("Opa!")
+    console.log(router);
 
     return <>
         <Navbar/>
--- components/my-projects/MyProjects.jsx
+++ components/my-projects/MyProjects.jsx
@@ -1,7 +1,7 @@
 import PageContent from '../pages-content/PageContent';
 
 function MyProjects() {
-    return <PageContent titlePage="My Projects" id="my-projects" notClicking/>
+    return <PageContent titlePage="My Projects" id="my-projects"/>
 };
 
 export default MyProjects;
