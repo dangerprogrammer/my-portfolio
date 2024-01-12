7b7ccaee137e1f4b657a0461f087fe0623116011
Patrick <104269678+dangerprogrammer@users.noreply.github.com>
2024-01-12 09:01:50 -0300

    .

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -16,7 +16,7 @@ function Navbar({ ...contexts }) {
             <PreLoader transitionDelay='.05s' backgroundColor='red' zIndex='1'/>
             <PreLoader transitionDelay='.1s' backgroundColor='white' zIndex='0'/>
         </span>
-        {pathname.slice(1) ? <p onClick={() => goTo({url: '/', ...contexts})} className={titleNav}><h1>Portfolio</h1></p> : <h1 className={titleNav}>Portfolio</h1>}
+        {pathname.slice(1) ? <h1 onClick={() => goTo({url: '/', ...contexts})} className={titleNav} style={{cursor: 'pointer'}}>Portfolio</h1> : <h1 className={titleNav}>Portfolio</h1>}
         <ul className={socialMedia}>
             {socialMedias.map(({ link, Icon }, ind) => <li key={ind} className={mediaContainer} style={{animationDelay: `${5e2 + ind * 1e2}ms`}}>
                 <Link href={link} target='_blank'><Icon/></Link>
