c43ee6c357041a287e53d2568014763de6f5569d
Patrick <104269678+dangerprogrammer@users.noreply.github.com>
2024-01-12 10:53:23 -0300

    .

--- components/pages-content/PageContent.jsx
+++ components/pages-content/PageContent.jsx
@@ -1,11 +1,12 @@
-import { pageContent, imageContainer, preLoaders, contentImage, mainTitle } from './PageContent.module.scss';
+import { pageContent, imageContainer, preLoaders, contentImage, mainTitle, space } from './PageContent.module.scss';
 import goTo from '@/tools/goTo';
 
 function PageContent({ titlePage, children, id, ...contexts }) {
     return <section {...{id, ...contexts}} style={{display: 'flex'}}>
         <main className={pageContent} style={{zIndex: 50}}>
             <h1 className={mainTitle}>
-                <span>{titlePage}</span>
+                {/* <span>{titlePage}</span> */}
+                <div>{[...titlePage].map((l, ind) => <span className={l == ' ' ? space : undefined} style={{transitionDelay: `calc(${titlePage.length - ind}ms * 35)`}}>{l}</span>)}</div>
             </h1>
             <aside className={imageContainer} onClick={() => goTo({url: `/${id}`, prevURL: `/#${id}`, ...contexts})}>
                 <div className={preLoaders} style={{'--delay': '.15s', backgroundColor: 'black', zIndex: 2}}></div>
