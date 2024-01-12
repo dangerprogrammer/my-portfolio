70e7eb875bbcaa2ec55c8f6a72386eceaf588fce
Patrick <104269678+dangerprogrammer@users.noreply.github.com>
2024-01-12 10:41:12 -0300

    .

--- components/pages-content/PageContent.jsx
+++ components/pages-content/PageContent.jsx
@@ -4,7 +4,9 @@ import goTo from '@/tools/goTo';
 function PageContent({ titlePage, children, id, ...contexts }) {
     return <section {...{id, ...contexts}} style={{display: 'flex'}}>
         <main className={pageContent} style={{zIndex: 50}}>
-            <h1 className={mainTitle}>{titlePage}</h1>
+            <h1 className={mainTitle}>
+                <span>{titlePage}</span>
+            </h1>
             <aside className={imageContainer} onClick={() => goTo({url: `/${id}`, prevURL: `/#${id}`, ...contexts})}>
                 <div className={preLoaders} style={{'--delay': '.15s', backgroundColor: 'black', zIndex: 2}}></div>
                 <div className={preLoaders} style={{'--delay': '.075s', backgroundColor: 'red', zIndex: 1}}></div>
