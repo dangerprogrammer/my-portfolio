7f7f282136134a2d18642fbbd32a96ed113fb04b
Patrick <104269678+dangerprogrammer@users.noreply.github.com>
2024-01-18 08:53:29 -0300

    .

--- components/context/ContextApp.jsx
+++ components/context/ContextApp.jsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { usePathname, useRouter } from "next/navigation";
 import { useState, createContext } from "react";
 
 const ContextApp = createContext();
@@ -10,26 +9,7 @@ function ContextProvider({ children }) {
         values = { history, setHistory },
         { Provider } = ContextApp;
 
-    function backHistory(size = 1) {
-        const { push } = useRouter(),
-            pathname = usePathname();
-
-        for (let i = history.length - 1 - size; i >= 0; i--) {
-            const route = history[i];
-
-            if (route !== pathname) {
-                push(route);
-
-                const newHistory = history.slice(0, Math.max(0, history.length - 1 - size - i));
-
-                setHistory(newHistory);
-
-                break;
-            };
-        };
-    };
-
-    return <Provider value={{ ...values, backHistory }}>
+    return <Provider value={{ ...values }}>
         { children }
     </Provider>
 };
