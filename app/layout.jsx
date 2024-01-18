11bf0c7972c5ff1d11ba920bc236fe4c205da528
Patrick <104269678+dangerprogrammer@users.noreply.github.com>
2024-01-18 09:12:31 -0300

    .

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,9 +12,13 @@ const metadata = {
 export { metadata };
 
 function RootLayout({ children }) {
-  return <>
-    {children}
-  </>
+  return <html lang="pt-BR">
+    <body>
+      <ContextProvider>
+        {/* {children} */}
+      </ContextProvider>
+    </body>
+  </html>
 };
 
 export default RootLayout;
