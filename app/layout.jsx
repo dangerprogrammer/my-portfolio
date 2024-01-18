fd1f927db4dafb0fa7607092db76051f78445c3d
Patrick <104269678+dangerprogrammer@users.noreply.github.com>
2024-01-18 08:44:56 -0300

    .

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,15 @@
 import '@/styles/global.scss';
 import ContextProvider from '@/components/context/ContextApp';
 
-// const metadata = {
-//   title: {
-//     template: '%s | Patrick',
-//     default: 'Patrick Portfolio'
-//   },
-//   description: 'A website for my portfolio',
-// };
+const metadata = {
+  title: {
+    template: '%s | Patrick',
+    default: 'Patrick Portfolio'
+  },
+  description: 'A website for my portfolio',
+};
 
-// export { metadata };
+export { metadata };
 
 function RootLayout({ children }) {
   return (
